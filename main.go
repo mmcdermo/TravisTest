3882d7ed2ee8f8b8b1e21b8005d10547207022ea
Morgan McDermott <mmcdermo@Morgans-MacBook-Pro.local>
2017-01-08 13:05:06 -0800

    A new test endpoint

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request){
-	z := "<HTML><HEAD><TITLE>INDEX</TITLE></HEAD><BODY>Hello, World. I have been reborn!</BODY></HTML>"
+	z := "<HTML><HEAD><TITLE>INDEX</TITLE></HEAD><BODY>Hello, World. I have been reborn yet again!</BODY></HTML>"
 	fmt.Fprintf(w, "%s", z)
 }
 
