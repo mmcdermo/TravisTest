b167092b7bd35c1ba59f649969f65d0a4e9128f8
Morgan McDermott <mmcdermo@Morgans-MacBook-Pro.local>
2017-01-08 19:46:27 -0800

    Prodtest

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request){
-	z := "<HTML><HEAD><TITLE>INDEX</TITLE></HEAD><BODY>I should be on Dev only!!231451</BODY></HTML>"
+	z := "<HTML><HEAD><TITLE>INDEX</TITLE></HEAD><BODY>New production rollout test</BODY></HTML>"
 	fmt.Fprintf(w, "%s", z)
 }
 
